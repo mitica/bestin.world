4b719b1f3d1547ea27edbf52a816912e3a483078
Dumitru C. <canteadumitru@gmail.com>
2025-06-21 13:58:05 +0300

    Update helpers.ts

--- scripts/common/helpers.ts
+++ scripts/common/helpers.ts
@@ -7,50 +7,62 @@ import type {
   TopicInfo
 } from "../../src/content/common/types";
 
+let countries: CountryInfo[];
+
 export const getCountries = async () => {
-  const countries = await readFile(
-    "src/content/common/countries.json",
-    "utf-8"
-  ).then((data) => JSON.parse(data));
-  return countries as CountryInfo[];
+  if (countries) return countries;
+
+  countries = await readFile("src/content/common/countries.json", "utf-8").then(
+    (data) => JSON.parse(data)
+  );
+  return countries;
 };
 
+let indicators: IndicatorInfo[];
+
 export const getIndicators = async () => {
-  const indicators = await readFile(
+  if (indicators) return indicators;
+  indicators = await readFile(
     "src/content/common/indicators.json",
     "utf-8"
   ).then((data) => JSON.parse(data));
-  return indicators as IndicatorInfo[];
+  return indicators;
 };
+
+let topics: TopicInfo[];
 
 export const getTopics = async () => {
-  const topics = await readFile("src/content/common/topics.json", "utf-8").then(
+  if (topics) return topics;
+  topics = await readFile("src/content/common/topics.json", "utf-8").then(
     (data) => JSON.parse(data)
   );
-  return topics as TopicInfo[];
+  return topics;
 };
+
+let languages: LanguageInfo[];
 
 export const getLanguages = async () => {
-  const languages = await readFile(
-    "src/content/common/languages.json",
-    "utf-8"
-  ).then((data) => JSON.parse(data));
-  return languages as LanguageInfo[];
+  if (languages) return languages;
+  languages = await readFile("src/content/common/languages.json", "utf-8").then(
+    (data) => JSON.parse(data)
+  );
+  return languages;
 };
 
+let wbIndicators: IndicatorCountryValue[];
+
 export const getWBIndicatorData = async (indicator: IndicatorInfo) => {
-  const data = await readFile(
+  if (wbIndicators) return wbIndicators;
+  const data: {
+    country: { id: string };
+    date: string;
+    value: string | null;
+    decimal: number;
+  }[] = await readFile(
     `data/wb/wb-indicator-${indicator.idWorldBank}.json`,
     "utf-8"
   ).then((data) => JSON.parse(data));
-  return (
-    data as {
-      country: { id: string };
-      date: string;
-      value: string | null;
-      decimal: number;
-    }[]
-  )
+  wbIndicators = data
     .filter((item) => !!item.date && !!item.value && item.value !== "null")
     .map<IndicatorCountryValue>((item) => ({
       countryId: item.country.id.toString().toLowerCase(),
@@ -60,6 +72,8 @@ export const getWBIndicatorData = async (indicator: IndicatorInfo) => {
       indicatorId: indicator.id,
       type: "average" // Default type, can be changed later
     }));
+
+  return wbIndicators;
 };
 
 /**
