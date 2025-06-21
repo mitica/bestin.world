1087ce8ed1370a260114079970c24bd974ea5e3f
Dumitru C. <canteadumitru@gmail.com>
2025-06-21 13:42:50 +0300

    fixes

--- src/generated-locales.ts
+++ src/generated-locales.ts
@@ -1,51 +1,70 @@
-import { Locales, Translator, type TranslatorOptions } from "localizy";
 
-export class LocalizyLocalesProvider<
-  T extends LocalizyLocales = LocalizyLocales
-> {
-  private translator: Translator;
-  private localesMap: { [lang: string]: T } = {};
+import { Locales, Translator, type TranslatorOptions } from 'localizy';
 
-  constructor(options: TranslatorOptions) {
-    this.translator = new Translator(options);
-  }
+export class LocalizyLocalesProvider<T extends LocalizyLocales = LocalizyLocales> {
+    private translator: Translator
+    private localesMap: { [lang: string]: T } = {}
 
-  lang(lang: string) {
-    if (!this.localesMap[lang]) {
-      this.localesMap[lang] = this.createInstance(
-        this.translator.locales(lang)
-      ) as T;
+    constructor(options: TranslatorOptions) {
+        this.translator = new Translator(options);
     }
 
-    return this.localesMap[lang];
-  }
+    lang(lang: string) {
+        if (!this.localesMap[lang]) {
+            this.localesMap[lang] = this.createInstance(this.translator.locales(lang)) as T;
+        }
 
-  protected createInstance(t: Locales): T {
-    return new LocalizyLocales(t) as T;
-  }
+        return this.localesMap[lang];
+    }
+
+    protected createInstance(t: Locales): T {
+        return new LocalizyLocales(t) as T;
+    }
 }
 
 export class LocalizyLocales {
-  protected __locales: Locales;
-  constructor(locales: Locales) {
-    this.__locales = locales;
-  }
+    protected __locales: Locales
+    constructor(locales: Locales) {
+        this.__locales = locales;
+    }
 
-  s(key: LocalesKey, ...args: any[]) {
-    return this.v(key, args);
-  }
+    s(key: LocalesKey, ...args: any[]) {
+        return this.v(key, args);
+    }
 
-  v(key: LocalesKey, args?: any[]) {
-    return this.__locales.t(key, args);
-  }
+    v(key: LocalesKey, args?: any[]) {
+        return this.__locales.t(key, args);
+    }
+    
 
-  home_page_title() {
-    return this.v("home_page_title");
-  }
+    home_page_title() {
+        return this.v('home_page_title');
+    }
 
-  home_page_description() {
-    return this.v("home_page_description");
-  }
+    home_page_description() {
+        return this.v('home_page_description');
+    }
+
+    country_vs_country_title(_p1: string, _p2: string) {
+        return this.v('country_vs_country_title', Array.from(arguments));
+    }
+
+    country_vs_country_description(_p1: string, _p2: string) {
+        return this.v('country_vs_country_description', Array.from(arguments));
+    }
+
+    topic_page_title(_p1: string) {
+        return this.v('topic_page_title', Array.from(arguments));
+    }
+
+    topic_page_description(_p1: string) {
+        return this.v('topic_page_description', Array.from(arguments));
+    }
 }
 
-export type LocalesKey = "home_page_title" | "home_page_description";
+export type LocalesKey = 'home_page_title'
+    | 'home_page_description'
+    | 'country_vs_country_title'
+    | 'country_vs_country_description'
+    | 'topic_page_title'
+    | 'topic_page_description';
