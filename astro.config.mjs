6ffe0e924ab5a466638042fbb3a1bc619b74926c
Dumitru C. <canteadumitru@gmail.com>
2025-06-21 10:38:17 +0300

    Remove React

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://bestin.world",
@@ -20,7 +18,5 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  }
 });
