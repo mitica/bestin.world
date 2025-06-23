791b040038f6cbd2a1dd6884de169d26c99acb23
Dumitru C. <canteadumitru@gmail.com>
2025-06-23 13:22:08 +0300

    better locales

--- src/generated-locales.ts
+++ src/generated-locales.ts
@@ -60,6 +60,22 @@ export class LocalizyLocales {
     topic_page_description(_p1: string) {
         return this.v('topic_page_description', Array.from(arguments));
     }
+
+    country_page_title(_p1: string) {
+        return this.v('country_page_title', Array.from(arguments));
+    }
+
+    country_page_description(_p1: string) {
+        return this.v('country_page_description', Array.from(arguments));
+    }
+
+    country_in_the_lead(_p1: string) {
+        return this.v('country_in_the_lead', Array.from(arguments));
+    }
+
+    country_folling_behind(_p1: string) {
+        return this.v('country_folling_behind', Array.from(arguments));
+    }
 }
 
 export type LocalesKey = 'home_page_title'
@@ -67,4 +83,8 @@ export type LocalesKey = 'home_page_title'
     | 'country_vs_country_title'
     | 'country_vs_country_description'
     | 'topic_page_title'
-    | 'topic_page_description';
+    | 'topic_page_description'
+    | 'country_page_title'
+    | 'country_page_description'
+    | 'country_in_the_lead'
+    | 'country_folling_behind';
