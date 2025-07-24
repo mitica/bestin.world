41ce6818c92e92338f3065b85d0914b6b9642547
Dumitru C. <canteadumitru@gmail.com>
2025-07-24 12:09:06 +0300

    country summary use main indicators only

--- scripts/country/country-summary.ts
+++ scripts/country/country-summary.ts
@@ -6,17 +6,18 @@ import {
 } from "../common/helpers";
 import type { CountrySummary } from "../../src/content/common/types";
 import { fileURLToPath } from "url";
+import { MAIN_INDICATOR_IDS } from "../../src/config";
 
 export async function generate() {
   const indicators = await getIndicators();
   const indicatorIds = indicators
-    .filter((it) => it.isComparable == true)
+    .filter((it) => MAIN_INDICATOR_IDS.includes(it.id))
     .map((it) => it.id);
   const countries = await getCountries();
   const ranks = await readIndicatorRanks();
   const results: CountrySummary[] = [];
   const fileName = `src/content/country/summary.json`;
-  const getIndicator = (id: string) => indicators.find((it) => it.id === id)!;
+  // const getIndicator = (id: string) => indicators.find((it) => it.id === id)!;
   for (const country of countries) {
     const countryRanks = ranks.filter(
       (rank) =>
@@ -26,19 +27,14 @@ export async function generate() {
       results.push({
         countryId: country.id,
         rank: 0,
-        points: countryRanks.reduce(
-          (sum, rank) =>
-            sum +
-            rank.rank * ((getIndicator(rank.indicatorId).priority || 0) + 1),
-          0
-        ),
+        points: countryRanks.reduce((sum, rank) => sum + rank.rank, 0),
         indicatorCount: countryRanks.length
       });
     }
   }
   results.sort(
-    (a, b) => a.points - b.points
-    // (a, b) => a.points / a.indicatorCount - b.points / b.indicatorCount
+    // (a, b) => a.points - b.points
+    (a, b) => a.points / a.indicatorCount - b.points / b.indicatorCount
   );
   results.forEach((result, index) => {
     result.rank = index + 1;
